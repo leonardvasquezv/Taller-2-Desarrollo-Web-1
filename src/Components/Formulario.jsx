814e2602950d4a9f7cba48f6aa1c8ce9608edd44
Leonard Vasquez <leonardvasquez22@gmail.com>
2022-04-19 13:27:10 -0500

    Creando diseño Listado de Empleados

--- src/Components/Formulario.jsx
+++ src/Components/Formulario.jsx
@@ -13,38 +13,38 @@ const Formulario = () => {
   const guardarEmpleado = (e) => {
     e.preventDefault()
 
-    if (!nombre.trim()){
+    if (!nombre.trim()) {
       alert('Digite el nombre')
       return
     }
-    if (!cedula.trim()){
+    if (!cedula.trim()) {
       alert('Digite el cedula')
       return
     }
-    if (!edad.trim()){
+    if (!edad.trim()) {
       alert('Digite el edad')
       return
     }
-    if (!email.trim()){
+    if (!email.trim()) {
       alert('Digite el email')
       return
     }
-    if (!telefono.trim()){
+    if (!telefono.trim()) {
       alert('Digite el telefono')
       return
     }
-    if (!tiempo.trim()){
+    if (!tiempo.trim()) {
       alert('Digite el tiempo')
       return
     }
-    if (!salario.trim()){
+    if (!salario.trim()) {
       alert('Digite el salario')
       return
     }
 
     setListaEmpleados([
       ...listaEmpleados,
-      
+
       { aNombre: nombre, aCedula: cedula, aEdad: edad, aEmail: email, aTelefono: telefono, aTiempo: tiempo, aSalario: salario }
     ])
 
@@ -61,7 +61,8 @@ const Formulario = () => {
 
   return (
     <div className="container mt-5">
-      <h2>Empleados</h2>
+      <h1 className="text-center">Administrar Empleados</h1>
+      <hr />
       <form onSubmit={guardarEmpleado}>
         <input className="form-control mb-2" type="text" placeholder="Ingrese Nombre"
           onChange={(e) => setNombre(e.target.value)}
@@ -81,18 +82,28 @@ const Formulario = () => {
         <input className="form-control mb-2" type="text" placeholder="Ingrese Tiempo"
           onChange={(e) => setTiempo(e.target.value)}
         />
-        <input className="form-control mb|-2" type="text" placeholder="Ingrese Salario"
+        <input className="form-control mb-2" type="text" placeholder="Ingrese Salario"
           onChange={(e) => setSalario(e.target.value)}
         />
         <button className="btn btn-primary btn-block" type="submit">Agregar</button>
       </form>
-      {
-        listaEmpleados.map((item, index) => (
-          <div key={index}>
-            <h4>{item.aNombre}-{item.aCedula}-{item.aEdad}-{item.aEmail}-{item.aTelefono}-{item.aTiempo}-{item.aSalario}</h4>
-          </div>
-        ))
-      }
+
+      <div className="row mt-5">
+        <div className="col-12">
+          <h4 className="text-center">Listado de Empleados</h4>
+          <ul className="list-group">
+            {
+              listaEmpleados.map((item, index) => (
+                <li className="list-group-item" key={index}>
+                  <spam className="lead">{item.aNombre} - {item.aCedula} - {item.aEdad} - {item.aEmail} - {item.aTelefono} - {item.aTiempo} - {item.aSalario}</spam>
+                  <button className="btn btn-danger btn-sm float-end mx-2">Eliminar</button>
+                  <button className="btn btn-warning btn-sm float-end mx-2">Editar</button>
+                </li>
+              ))
+            }
+          </ul>
+        </div>
+      </div>
     </div>
   );
 };
